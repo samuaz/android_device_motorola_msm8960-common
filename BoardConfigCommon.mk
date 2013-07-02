1eebdc7b63554e0d93b035203e686f5e4b17228a
dhacker29 <davidhackerdvm@gmail.com>
2013-07-01 21:20:41 -0500

    Update board flags/optimize

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,9 +41,12 @@ TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_VARIANT := cortex-a9
 TARGET_CPU_SMP := true
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-BOARD_USES_QCOM_HARDWARE := true
+#TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+#TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_EXTRA_CFLAGS := -mtune=cortex-a9 -mcpu=cortex-a9 -fno-inline-functions -fno-ipa-cp-clone -fno-unswitch-loops -fno-tree-vectorize
+TARGET_GCC_VERSION := 4.7
+TARGET_USE_O3 := true
+TARGET_USE_LINARO_STRING_ROUTINES := true
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
