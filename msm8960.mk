0eb32ddb42f5ebc19e508f628698fab83aebb96e
dhacker29 <davidhackerdvm@gmail.com>
2013-07-09 04:10:15 -0500

    Revert "Enable tunnel audio"
    This reverts commit 77c8d84b1dd46c5c177f0f8cd600a59a8bcfb2e4.

--- msm8960.mk
+++ msm8960.mk
@@ -245,9 +245,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    lpa.decode=false \
-    tunnel.decode=true \
-    tunnel.audiovideo.decode=true \
+    lpa.decode=true \
     lpa.use-stagefright=true \
     media.stagefright.enable-player=true \
     media.stagefright.enable-http=true \
