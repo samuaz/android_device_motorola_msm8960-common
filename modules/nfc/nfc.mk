447fd808737a351b7e7918e7d01d1f4806658c08
Hashcode <hashcode0f@gmail.com>
2013-07-08 22:32:20 -0700

    use local nfc permissions

--- modules/nfc/nfc.mk
+++ modules/nfc/nfc.mk
@@ -8,9 +8,9 @@ else
 endif
 
 PRODUCT_COPY_FILES += \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+    $(LOCAL_PATH)/modules/nfc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    $(LOCAL_PATH)/modules/nfc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    $(LOCAL_PATH)/modules/nfc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
 # NFC Support
 PRODUCT_PACKAGES += \
