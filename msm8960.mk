CyanogenMod/android_device_motorola_moto_msm8960

root: Fix up bluetooth MAC address
just running btnvtool was not enough to set the Bluetooth MAC
properly so we had a dummy address everytime. Run the init.qcom.bt
without onboot and it fully configures and sets proper MAC

Change-Id: Ieead38a927371e42ccf87faf2a5633f408386368

msm8960.mk
@@ -130,6 +130,8 @@ PRODUCT_PACKAGES += \
 
 # Init scripts
 PRODUCT_PACKAGES += \
+    init.crda.sh \
+    init.qcom.bt.sh \
     init.qcom.class_core.sh \
     init.qcom.class_main.sh \
     init.qcom.coex.sh \
@@ -141,8 +143,7 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sh \
     init.qcom.syspart_fixup.sh \
-    init.qcom.usb.sh \
-    init.crda.sh
+    init.qcom.usb.sh
 
 # Scripts
 PRODUCT_COPY_FILES += \

samuaz/android_device_motorola_msm8960-common

Conflicts:
	msm8960.mk

msm8960.mk
@@ -127,6 +127,8 @@ PRODUCT_PACKAGES += \
 
 # Init scripts
 PRODUCT_PACKAGES += \
+    init.crda.sh \
+    init.qcom.bt.sh \
     init.qcom.class_core.sh \
     init.qcom.class_main.sh \
     init.qcom.coex.sh \
@@ -139,8 +141,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.syspart_fixup.sh \
     init.qcom.usb.sh \
-    init.qcom.wifi.sh \
-    init.crda.sh
+    init.qcom.wifi.sh
 
 # Scripts
 PRODUCT_COPY_FILES += \