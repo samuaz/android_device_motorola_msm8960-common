4b0b10d474c005e46100ac973f83f7490319c35c
dhacker29 <davidhackerdvm@gmail.com>
2013-07-28 18:40:04 -0500

    Revert "Temp: copy ubuntu-boot to system/etc for flashing"
    This reverts commit 364c89accc4e1aad29636fe406a694ad5ff3b4c1.

--- msm8960.mk
+++ msm8960.mk
@@ -153,10 +153,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
 
-# Ubuntu bootimage
-PRODUCT_COPY_FILES += \
-    $(OUT)/ubuntu-boot.img:/system/etc/ubuntu-boot.img
-
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/audio_policy.conf:system/etc/audio_policy.conf \
